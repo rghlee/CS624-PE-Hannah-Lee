e736a95edf3375eca5da17720b69901c83d68a7c
rghlee <140765098+rghlee@users.noreply.github.com>
2026-03-02 10:24:49 +0000

    PE04 Part2 Profile Cards- Hannah

--- Styling/app/(tabs)/App.js
+++ Styling/app/(tabs)/App.js
@@ -1,36 +1,83 @@
 import React, { Component } from 'react';
-import { Image, Platform, StyleSheet, Text, View } from 'react-native';
-import ProfileCard from '../../components/ProfileCard';  // Import ProfileCard correctly
-
-const profileCardColor = 'dodgerblue';
+import { View, FlatList, StyleSheet } from 'react-native';
+import ProfileCard from '../../components/ProfileCard';  // Import ProfileCard component
 
 export default class App extends Component {
   constructor(props) {
     super(props);
-    // Initial state, where the profile card doesn't shrink by default
+    // Initial profiles data with showThumbnail state for each card
     this.state = {
-      showThumbnail: false, // Set this to true to test shrinking
+      profiles: [
+        {
+          image: require('../../assets/images/user.png'),
+          name: 'John Doe',
+          occupation: 'React Native Developer',
+          description: 'John is a really great Javascript developer. He loves using JS to build React Native applications for iOS and Android.',
+          showThumbnail: false,  // Initially not shrunk
+        },
+        {
+          image: require('../../assets/images/user.png'),
+          name: 'John Doe',
+          occupation: 'React Native Developer',
+          description: 'John is a really great Javascript developer. He loves using JS to build React Native applications for iOS and Android.',
+          showThumbnail: false,  // Initially not shrunk
+        },
+        {
+          image: require('../../assets/images/user.png'),
+          name: 'John Doe',
+          occupation: 'React Native Developer',
+          description: 'John is a really great Javascript developer. He loves using JS to build React Native applications for iOS and Android.',
+          showThumbnail: false,  // Initially not shrunk
+        },
+        {
+          image: require('../../assets/images/user.png'),
+          name: 'John Doe',
+          occupation: 'React Native Developer',
+          description: 'John is a really great Javascript developer. He loves using JS to build React Native applications for iOS and Android.',
+          showThumbnail: false,  // Initially not shrunk
+        },
+        {
+          image: require('../../assets/images/user.png'),
+          name: 'John Doe',
+          occupation: 'React Native Developer',
+          description: 'John is a really great Javascript developer. He loves using JS to build React Native applications for iOS and Android.',
+          showThumbnail: false,  // Initially not shrunk
+        },
+        {
+          image: require('../../assets/images/user.png'),
+          name: 'John Doe',
+          occupation: 'React Native Developer',
+          description: 'John is a really great Javascript developer. He loves using JS to build React Native applications for iOS and Android.',
+          showThumbnail: false,  // Initially not shrunk
+        },
+      ],
     };
   }
 
-  // Toggle the showThumbnail state when the card is pressed
-  handleProfileCardPress = () => {
-    this.setState(prevState => ({
-      showThumbnail: !prevState.showThumbnail, // Toggle between true/false
-    }));
+  // Handle profile card press to toggle expansion/collapse
+  handleProfileCardPress = (index) => {
+    const updatedProfiles = [...this.state.profiles];
+    updatedProfiles[index].showThumbnail = !updatedProfiles[index].showThumbnail;  // Toggle the showThumbnail state
+    this.setState({ profiles: updatedProfiles });
   };
 
   render() {
     return (
       <View style={styles.container}>
-        {/* Pass showThumbnail and onPress to ProfileCard */}
-        <ProfileCard
-          image={require('../../assets/images/user.png')}
-          name="John Doe"
-          occupation="React Native Developer"
-          description="John is a really great Javascript developer. He loves using JS to build React Native applications for iOS and Android."
-          showThumbnail={this.state.showThumbnail} // Shrink when true
-          onPress={this.handleProfileCardPress} // Toggle on press
+        <FlatList
+          data={this.state.profiles}
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={({ item, index }) => (
+            <ProfileCard
+              image={item.image}
+              name={item.name}
+              occupation={item.occupation}
+              description={item.description}
+              onPress={() => this.handleProfileCardPress(index)}  // Handle click to expand/collapse
+              showThumbnail={item.showThumbnail}  // Pass the showThumbnail state
+            />
+          )}
+          numColumns={2}  // Display the profile cards in 2 columns
         />
       </View>
     );
@@ -42,6 +89,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    paddingTop: 20,
   },
-  // Other card styles
 });
